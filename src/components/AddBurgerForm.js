1e9da2dc9ba9292724e29c3f8531eaa2cfcb0b82
sergWP <kuzmenko.sergei@gmail.com>
2021-11-10 08:56:41 +0200

    добавлена форма, запись в данных формы в state

---
+++ src/components/AddBurgerForm.js
@@ -0,0 +1,46 @@
+import React from 'react'
+
+class AddBurgerForm extends React.Component {
+
+    //ref-ссылки на поля формы
+    nameRef = React.createRef();
+    priceRef = React.createRef();
+    statusRef = React.createRef();
+    descRef = React.createRef();
+    imageRef = React.createRef();
+
+    createBurger = (event) => {
+        event.preventDefault();
+        //console.log('add burger', this.nameRef.current.value);
+        const burger = {
+            name: this.nameRef.current.value,
+            price: parseFloat(this.priceRef.current.value),
+            status: this.statusRef.current.value,
+            desc: this.descRef.current.value,
+            image: this.imageRef.current.value
+        }
+
+        //console.log(burger)
+        this.props.addBurger(burger);
+        // очищаем форму
+        event.currentTarget.reset();
+    }
+
+    render() {
+        return (
+            <form className={'burger-edit'} onSubmit={this.createBurger}>
+                <input ref={this.nameRef} name={'name'} type={'text'} placeholder={'Name'} autoComplete={'off'} />
+                <input ref={this.priceRef} name={'price'} type={'text'} placeholder={'Price'} autoComplete={'off'} />
+                <select ref={this.statusRef} name={'status'} className={'status'}>
+                    <option value={'avalible'}>Доступно</option>
+                    <option value={'unavalible'}>Убрать из меню</option>
+                </select>
+                <textarea ref={this.descRef} name={'desc'} placeholder={'Desc'} />
+                <input ref={this.imageRef} name={'image'} type={'text'} placeholder={'Image'} autoComplete={'off'} />
+                <button type={'submit'}>Добавить в меню</button>
+            </form>
+        )
+    }
+}
+
+export default AddBurgerForm
--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,29 @@ import Order from "./Order";
 import MenuAdmin from "./MenuAdmin";
 
 class App extends React.Component {
+    state = {
+        burgers: {},
+        order: {}
+    }
+
+    addBurger = (burger) => {
+        //console.log('addBurger', burger);
+        // 1. делаем копию state
+        const burgers = {...this.state.burgers};
+        // 2. добавить новый бургер в объект burgers
+        burgers[`burger${Date.now()}`] = burger;
+        // 3. записать обновленный burgers в объект state
+        this.setState({burgers: burgers});           // ES6 - this.setState({burgers})
+    }
+
     render() {
         return (
             <div className={'burger-paradise'}>
                 <div className={'menu'}>
-                    <Header title={'Very Hot Burger'} amount={10} />
+                    <Header title={'Very Hot Burger'} amount={2} />
                 </div>
                 <Order />
-                <MenuAdmin />
+                <MenuAdmin addBurger={this.addBurger} />
             </div>
         )
     }
